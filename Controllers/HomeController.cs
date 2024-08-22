d16237e4072dcc12b546f596275dc5c189507681
IvanDemkin <zaphta@gmail.com>
2024-08-22 11:58:16 +0700

    day3 lab1

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,16 +7,18 @@ namespace MyCoins.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
-
+    public CoinsData coinsList { get; set; }
+    
     public HomeController(ILogger<HomeController> logger)
     {
-        _logger = logger;
+        _logger = logger;      
+        this.coinsList = new CoinsData();      
     }
+        
 
     public IActionResult Index()
-    {
-        CoinsData model = new CoinsData();
-        return View(model.Coins);
+    {      
+        return View(this.coinsList.Coins);
     }
 
     public IActionResult Privacy()
@@ -24,7 +26,19 @@ public class HomeController : Controller
         return View();
     }
 
+    [Route("Save")]
+    [HttpPost]
+    public IActionResult Save(Coin element)
+    {
+        this.coinsList.AddCoin(element);
+        return View("Index", this.coinsList.Coins);
+    }
 
+    [Route("addCoin")]
+    public IActionResult AddCoin()
+    {
+        return View();
+    }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
